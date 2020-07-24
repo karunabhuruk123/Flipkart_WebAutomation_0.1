2b96acc6b788a96626c8c12685f5465b963c57d3
Lenovo <Lenovo@DESKTOP-AGBCM7S>
2020-07-24 04:26:04 -0700

    First commit

---
+++ E2ENewProject3/src/test/java/com/flipkart/qa/testclasses/AddToCartPageTest.java
@@ -0,0 +1,53 @@
+package com.flipkart.qa.testclasses;
+
+import java.util.Iterator;
+import java.util.Set;
+
+import org.openqa.selenium.support.pagefactory.AjaxElementLocatorFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import com.flipkart.qa.base.TestBase;
+import com.flipkart.qa.pages.AddToCartPage;
+import com.flipkart.qa.pages.HomePage;
+
+public class AddToCartPageTest extends TestBase {
+
+	AddToCartPage addToCart;
+	HomePage homePage;
+
+	@BeforeMethod
+	public void setUp() {
+
+		addToCart = new AddToCartPage();
+		homePage = new HomePage();
+
+	}
+
+	// This Test will add product to cart and place order
+	@Test
+	public void AddToCartTest() throws InterruptedException {
+		
+		
+
+
+		homePage.searchProduct();
+
+		homePage.selectRandomProduct();
+
+		Set<String> ids = driver.getWindowHandles();
+		Iterator<String> it = ids.iterator();
+		String parentId = it.next();
+		String childId = it.next();
+		driver.switchTo().window(childId);
+
+		addToCart.AddToCart();
+
+		addToCart.PlaceOrder();
+
+		driver.switchTo().window(parentId);
+
+	}
+
+}
---
+++ E2ENewProject3/src/test/java/com/flipkart/qa/testclasses/LoginPageTest.java
@@ -0,0 +1,35 @@
+package com.flipkart.qa.testclasses;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import com.flipkart.qa.base.TestBase;
+import com.flipkart.qa.pages.HomePage;
+import com.flipkart.qa.pages.LoginPage;
+
+public class LoginPageTest extends TestBase{
+	LoginPage loginPage;
+	HomePage homePage;
+	
+	public  LoginPageTest() {
+		super();
+	}
+	
+	
+	@BeforeMethod
+	public void setUp(){
+		initializeDriver();
+		loginPage=new LoginPage();
+	}
+	
+	
+	
+	
+	
+	
+	@AfterMethod
+	public void tearDown() {
+		driver.quit();
+
+	}
+
+}
